82066fdfe7de2494405b3e200c2c048c90534535
Charles Holtzkampf <charles.holtzkampf@gmail.com>
2019-08-30 10:31:26 +0100

    Update enable_ssh.sh

--- enable_ssh.sh
+++ enable_ssh.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
 # Add all IPs for miners in ips.txt
+# anitminer_ssh.sh username password 
 
 IPS=$(cat ips.txt)
+USER=$1
+PASS=$2
 
 for ip in $IPS; do
 	echo
 	if [[ -e "/usr/bin/compile_time" ]]; then
-		curl -s -X POST --digest --user root:root http://$ip/cgi-bin/create_log_backup.cgi -d "/null /config/dropbear -sf"
+		curl -s -X POST --digest --user $USER:$PASS http://$ip/cgi-bin/create_log_backup.cgi -d "/null /config/dropbear -sf"
 		sleep 0.5
-		curl -s -X GET --digest --user root:root http://$ip/cgi-bin/reboot.cgi &
+		curl -s -X GET --digest --user $USER:$PASS http://$ip/cgi-bin/reboot.cgi &
 	else
-		curl -s -X POST --digest --user root:root http://$ip/cgi-bin/create_log_backup.cgi -d "/null /config/dropbear -sf"
+		curl -s -X POST --digest --user $USER:$PASS http://$ip/cgi-bin/create_log_backup.cgi -d "/null /config/dropbear -sf"
 		sleep 0.5
-		curl -s -X GET --digest --user root:root http://$ip/cgi-bin/reboot.cgi &
+		curl -s -X GET --digest --user $USER:$PASS http://$ip/cgi-bin/reboot.cgi &
 	fi
 
 
