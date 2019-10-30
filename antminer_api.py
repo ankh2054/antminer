d01a12c1fc433be5eef3a1877050190356e82a04
Charles Holtzkampf <charles.holtzkampf@gmail.com>
2019-10-30 14:26:29 +0000

    Update antminer_api.py

--- antminer_api.py
+++ antminer_api.py
@@ -1,39 +1,49 @@
-
 #!/usr/bin/env python3
 
-# A simple script for managing cgminer devices
-# python3 miner3.py 192.168.88.250 summary
+# Copyright 2013 Setkeh Mkfr
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.  See COPYING for more details.
 
-import socket  # import the built-in package to open a TCP/IP socket to connect to miners
-import json  # use the JSON package to format messages sent to the miner
+#Short Python Example for connecting to The Cgminer API
+#Written By: setkeh <https://github.com/setkeh>
+#Thanks to Jezzz for all his Support.
+#NOTE: When adding a param with a pipe | in bash or ZSH you must wrap the arg in quotes
+#E.G "pga|0"
+
+
+#python miner.py 192.168.88.250 stats
+
+import socket
+import json
 import sys
+
+def linesplit(socket):
+        buffer = socket.recv(4096).decode("ascii").rstrip(' \t\r\n\0') 
+        done = False
+        while not done:
+                more = socket.recv(4096).decode("ascii").rstrip(' \t\r\n\0') 
+                if not more:
+                        done = True
+                else:
+                        buffer = buffer+more
+        if buffer:
+                return buffer
 
 api_command = sys.argv[2]
 data = {'command': api_command}
-
-def api_cmd(hostaddress=sys.argv[1]):  # take the sysarg passed host IP address and send it a status command
-
-    try:  # use the try command to help catch errors
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # use the socket package to create a new connection
-        sock.settimeout(5)  # set the socket timeout to five second
-        sock.connect((hostaddress, 4028))  # connect to the provided host
-
-        message = json.dumps(data)  # create the JSON formatted command using sysarg input
-        sock.sendto(message.encode(), (hostaddress, 4028))  # send the command to the miner
-        response = sock.recv(4096).decode("ascii").rstrip(' \t\r\n\0')  # receive and clean up the response
-
-        sock.shutdown(socket.SHUT_RDWR)  # send the shutdown to the socket so it closes the connection cleanly
-        sock.close()  # close our socket
-
-        response_decoded = json.loads(response)  # decode the JSON response so we can access the individual data later
-        print(response_decoded)  # print the entire response received
-
-    except Exception as e:
-        print("Error: " + str(e))  # if there is an error, print it
+api_ip = sys.argv[1]
 
 
-def main():
-    #check_status("192.168.88.250")
-    api_cmd()
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((api_ip, 4028))
+message = json.dumps(data)
+s.sendto(message.encode(), (api_ip, 4028))
 
-main()
+response = linesplit(s)
+print(response)
+response = json.loads(response)
+print(response)
+s.close()
